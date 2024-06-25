9bd6d5e1f9c59a5d501e38b297b2bbb76689a50d
ibnuAdzakir <iadzakir@gmail.com>
2024-06-25 21:15:47 +0700

    Kasih deskripsi

--- src/app/components/Testimonial.jsx
+++ src/app/components/Testimonial.jsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 const testimonialData = [
   {
     name: "Kaka",
-    image: "/placeholder-images/kaka.jpg",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    image: "/images/orang1.jpg",
+    description: "Very good services",
   },
   {
     name: "Kiki",
-    image: "/placeholder-images/kiki.jpg",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    image: "/images/orang2.jpg",
+    description: "Very comfy car",
   },
   {
     name: "Koko",
-    image: "/placeholder-images/koko.jpg",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    image: "/images/orang3.png",
+    description: "Affordable price",
   },
 ];
 
@@ -29,13 +29,11 @@ const Testimonial = () => {
       <span id="about"></span>
       <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
         <div className="container mx-auto">
-
           <div className="space-y-4 pb-12">
             <p className="text-3xl font-semibold text-center sm:text-4xl font-serif">
               What Our Clients Say About Us
             </p>
           </div>
-
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black dark:text-white">
             {testimonialData.map((testi, index) => (
@@ -43,12 +41,13 @@ const Testimonial = () => {
                 key={index}
                 className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12 dark:bg-white/20 bg-gray-100 duration-300 rounded-lg"
               >
-                <div className="grid place-items-center">
+                <div className="relative rounded-full overflow-hidden w-40 h-40 mx-auto">
                   <Image
                     src={testi.image}
                     alt={`Picture of ${testi.name}`}
-                    width={80}
-                    height={80}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
                     className="rounded-full"
                   />
                 </div>
