c8cab562000dc66633907f78a5fd7710dfecde9e
ibnuAdzakir <iadzakir@gmail.com>
2024-06-25 20:26:38 +0700

    perubahan image

--- src/app/components/Testimonial.jsx
+++ src/app/components/Testimonial.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image"; 
 
 const testimonialData = [
   {
     name: "Kaka",
-    image: "",
+    image: "/placeholder-images/kaka.jpg",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
   },
   {
     name: "Kiki",
-    image: "",
+    image: "/placeholder-images/kiki.jpg",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
   },
   {
     name: "Koko",
-    image: "",
+    image: "/placeholder-images/koko.jpg",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
   },
 ];
@@ -28,12 +29,13 @@ const Testimonial = () => {
       <span id="about"></span>
       <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
         <div className="container mx-auto">
-          {/* Header */}
+
           <div className="space-y-4 pb-12">
             <p className="text-3xl font-semibold text-center sm:text-4xl font-serif">
               What Our Clients Say About Us
             </p>
           </div>
+
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black dark:text-white">
             {testimonialData.map((testi, index) => (
@@ -42,10 +44,12 @@ const Testimonial = () => {
                 className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12 dark:bg-white/20 bg-gray-100 duration-300 rounded-lg"
               >
                 <div className="grid place-items-center">
-                  <img
-                    src="https://picsum.photos/200"
-                    alt=""
-                    className="rounded-full w-20 h-20"
+                  <Image
+                    src={testi.image}
+                    alt={`Picture of ${testi.name}`}
+                    width={80}
+                    height={80}
+                    className="rounded-full"
                   />
                 </div>
                 <div className="text-2xl">⭐⭐⭐⭐⭐</div>
